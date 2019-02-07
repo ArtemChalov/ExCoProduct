b841a9308fb6c962cfee031bf94dec51caea11d6
ArtemChalov <chalov81@mail.ru>
2019-02-07 13:16:17 +0500

    Added script that changes header height.

---
+++ ExCoProduct/Pages/DataBaseControl/DBIndex.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExCoProduct.Pages.DataBaseControl
+{
+    public class DBIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
